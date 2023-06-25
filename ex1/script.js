fbcbb5675500541f247806ed3c630233507e727c
Augusto Pires <mpires.augusto@gmail.com>
2023-06-25 18:40:47 -0300

    Adicionando script.js para utilização dos demais módulos

---
+++ ex1/script.js
@@ -0,0 +1,9 @@
+import { CalculaArray } from "./calculaMedia.js";
+import { MultiplicaArray } from "./multiplicaArray.js";
+import { SomaArray } from "./somaArray.js";
+
+let nums = [3, 5, 15, 18, 1, 36, 14, 7];
+
+SomaArray(nums);
+MultiplicaArray(nums);
+CalculaArray(nums);
